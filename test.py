d868c1bdd06c84287bd53cf5ad2d90a57bad462f
Davitchos <davitchos@gmail.com>
2025-10-05 09:07:24 +0200

    execution of jyptiter nb cells

--- test.py
+++ test.py
@@ -16,4 +16,3 @@ secret = input("Enter 4 numbers separated by spaces: ")
 secret_arr = list(map(int, secret.split()))
 game = MasterMind(np.array(secret_arr))
 simulation.play_with_visualizer(game=game, guesser=PvP_Engine(), max_turns=6, visualizer=TextVisualizer(game.positions, game.colors))
-
